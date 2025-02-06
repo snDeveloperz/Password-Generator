f9b9bfc83751db4f5d1fda017ec7c38a1dbc06a9
sn-developerr <neizvestnyhstepan@gmail.com>
2025-02-06 14:45:47 +0400

    Change: Password History, Delete history, translate to English language

--- script/script.js
+++ script/script.js
@@ -1,31 +1,40 @@
 document.getElementById('generate').addEventListener('click', function() {
     const length = document.getElementById('length').value;
-    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:',.<>?";
-    let password = "";
+    const password = generatePassword(length);
+    document.getElementById('password').value = password;
+    addToHistory(password);
+});
 
+function generatePassword(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+    let password = "";
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * charset.length);
         password += charset[randomIndex];
     }
+    return password;
+}
 
-    document.getElementById('password').value = password;
-});
+function addToHistory(password) {
+    const historyList = document.getElementById('password-history');
+    const listItem = document.createElement('li');
+    listItem.textContent = password;
+
+    // Создаем кнопку для удаления
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'delete-button'; 
+    deleteButton.onclick = function() {
+        historyList.removeChild(listItem);
+    };
+
+    listItem.appendChild(deleteButton);
+    historyList.appendChild(listItem);
+}
+
 
 document.getElementById('copy').addEventListener('click', function() {
     const passwordField = document.getElementById('password');
     passwordField.select();
     document.execCommand('copy');
-
-    // Изменение текста и отображение значка проверки
-    const tooltip = this.querySelector('.tooltip');
-    const checkmark = this.querySelector('.checkmark');
-    
-    tooltip.innerText = tooltip.getAttribute('data-text-end');
-    checkmark.style.display = 'block'; // Показываем значок проверки
-
-    // Сбрасываем текст через несколько секунд
-    setTimeout(() => {
-        tooltip.innerText = tooltip.getAttribute('data-text-initial');
-        checkmark.style.display = 'none'; // Скрываем значок проверки
-    }, 2000);
 });
